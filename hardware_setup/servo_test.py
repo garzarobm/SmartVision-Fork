dc44236092be3b723ee8614c26d99dd73ae1b2f8
garzarobm <garzarobm@gmail.com>
2024-03-08 20:04:52 -0600

    create serial communication test python file

--- hardware_setup/servo_test.py
+++ hardware_setup/servo_test.py
@@ -11,17 +11,3 @@ GPIO.setup(servo_pin, GPIO.OUT)
 # Set up the servo and start it
 servo = GPIO.PWM(servo_pin, 50)  # 50 Hz frequency
 servo.start(0)  # Initial duty cycle of 0
-
-try:
-    while True:
-        # Move the servo back and forth between 0 and 180 degrees
-        for duty_cycle in range(0, 10, 1):
-            servo.ChangeDutyCycle(duty_cycle)
-            time.sleep(0.5)
-        for duty_cycle in range(10, 0, -1):
-            servo.ChangeDutyCycle(duty_cycle)
-            time.sleep(0.5)
-except KeyboardInterrupt:
-    # Clean up the GPIO on interrupt
-    servo.stop()
-    GPIO.cleanup()
